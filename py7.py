50191ff45e76ee7671505aea8168f53eba25479c
Alisa Choi <cojalisa072@gmail.com>
2025-10-21 15:52:41 +0300

    Add files via upload

--- py7.py
+++ py7.py
@@ -4,11 +4,13 @@ from decouple import config
 
 
 TG_TOKEN = config('TELEGRAM_TOKEN')
-TG_CHAT_ID = config('TELEGRAM_CHAT_ID')
+chat_id = config('TELEGRAM_CHAT_ID')
+bot = ptbot.Bot(TG_TOKEN)
 
 
-def wait(chat_id, message):
+def wait(chat_id, message_id):
     message_id = bot.send_message(chat_id, 'Запускаю таймер...')
+    bot.register_next_step_handler(message_id, notify_progress)
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
@@ -23,12 +25,12 @@ def notify_progress(secs_left, chat_id, message_id, total_secs, bot):
     progressbar = render_progressbar(total_secs, total_secs - secs_left)
     bot.create_countdown(total_secs, notify_progress, chat_id=chat_id, message_id=message_id, total_secs=total_secs)
     bot.create_timer(total_secs, notify_progress, chat_id=chat_id, message_id=message_id)
-    bot.update_message(chat_id, message_id, f'Осталось секунд{secs_left}\n{render_progressbar}')
+    bot.update_message(f'Осталось секунд{secs_left}\n{render_progressbar}', chat_id=message.chat.id, message_id=message.message.id)
 
-def main(chat_id, message_id, bot):
-    bot.update_message(chat_id, message_id, "Время вышло")
+def main():
+    bot.reply_on_message(wait)
+    bot.send_message(chat_id, "Время вышло!")
+    bot.run()
 
 if __name__ == '__main__':
-    bot = ptbot.Bot(TG_TOKEN)
-    bot.reply_on_message(wait)
-    bot.run()
+    main()
