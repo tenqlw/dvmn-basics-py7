613fb7ae7b9edee5ae39ed183d2c03ec64a0069c
Alisa Choi <cojalisa072@gmail.com>
2025-10-24 11:02:55 +0300

    Add files via upload

--- py7.py
+++ py7.py
@@ -3,14 +3,6 @@ from pytimeparse import parse
 from decouple import config
 
 
-TG_TOKEN = config('TELEGRAM_TOKEN')
-chat_id = config('TELEGRAM_CHAT_ID')
-bot = ptbot.Bot(TG_TOKEN)
-
-
-def wait(chat_id, message_id):
-    message_id = bot.send_message(chat_id, 'Запускаю таймер...')
-    bot.register_next_step_handler(message_id, notify_progress)
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
@@ -20,17 +12,34 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     pbar = fill * filled_length + zfill * (length - filled_length)
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
+
 def notify_progress(secs_left, chat_id, message_id, total_secs, bot):
-    total_secs = parse(message_id)
     progressbar = render_progressbar(total_secs, total_secs - secs_left)
-    bot.create_countdown(total_secs, notify_progress, chat_id=chat_id, message_id=message_id, total_secs=total_secs)
-    bot.create_timer(total_secs, notify_progress, chat_id=chat_id, message_id=message_id)
-    bot.update_message(f'Осталось секунд{secs_left}\n{render_progressbar}', chat_id=message.chat.id, message_id=message.message.id)
+    bot.update_message(chat_id, message_id, f"Осталось секунд: {secs_left}\n{progressbar}")
+
+
+def last_message(bot, chat_id):
+    bot.send_message(chat_id, 'Время вышло!')
+
+
 
 def main():
+    TG_TOKEN = config('TELEGRAM_TOKEN')
+    TG_CHAT_ID = config('TELEGRAM_CHAT_ID')
+    bot = ptbot.Bot(TG_TOKEN)
+
+    def wait(chat_id, msg):
+        seconds = parse(msg)
+        if not seconds:
+            bot.send_message(chat_id, 'Неверный формат времени')
+            return
+        message_id = bot.send_message(chat_id, f'Таймер запущен на {seconds} секунд')
+        bot.create_countdown(seconds, notify_progress, chat_id=chat_id, message_id=message_id, total_secs=seconds, bot=bot)
+        bot.create_timer(seconds, last_message, chat_id=chat_id, bot=bot)
+        
     bot.reply_on_message(wait)
-    bot.send_message(chat_id, "Время вышло!")
-    bot.run()
+    bot.run_bot()
+
 
 if __name__ == '__main__':
     main()
